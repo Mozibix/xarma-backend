138f3dbf3f937885f2746808124df8784fa8c3f4
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2024-09-28 22:23:21 +0100

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,7 @@ const config = {
     LOG_LEVEL: process.env.LOG_LEVEL || 'info',
     LOG_FILE: process.env.LOG_FILE || 'X-ARME.log',
     HOSTNAME: process.env.HOSTNAME || 'localhost',
-    PORT: process.env.PORT || '8000',
+    PORT: process.env.PORT || '3000',
     URL: process.env.URL || 'http://localhost:8000',
     MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/xarme-test',
     NODE_ENV: process.env.NODE_ENV || 'development',
