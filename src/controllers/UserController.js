cd8d763c1f686bad63544acdac18693d7448d423
luminousbinary <boolwrld@outlook.com>
2024-10-17 17:28:18 +0100

    separated the user and rank controllers

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -75,31 +75,6 @@ class UserController {
   // 
 
 
-    /**
-   * @description get a user rank by userId
-   * @param  {object} req
-   * @param {object} res
-   * @returns {object} a json object
-   * @memberof UserController
-   */
-  static async getUserRankColor(req,res){
-    try {
-      const user = req.user;
-      const rankColordetails = await rankService.getRankColor(user._id);
-      const rankcolor = {
-        "color": rankColordetails
-      }
-
-      res.status(200).json({ status: true, message: "User Rank Color Retrieved Successfully", data: rankcolor })
-    } catch (error) {
-      return res.status(400).json({
-        success:false,
-        errorCode:121,
-        data: error.message
-
-      })
-    }
-  }
 }
 
 export default UserController
