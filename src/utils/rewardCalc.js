aabb1cfad72b01bf4bb293b1a6ebb70cf8ae4fa6
luminousbinary <boolwrld@outlook.com>
2024-10-17 17:46:50 +0100

    fix: a simplified multiplier

--- src/utils/rewardCalc.js
+++ src/utils/rewardCalc.js
@@ -5,26 +5,15 @@ const rewardCalc = (
   sniped
 ) => {
   //sniped is a boolean to determin wether or not the url has been sniped by a sniper
+  let gamaScore = numberOfImpression / 1000000;
+  let xeetScore = numberOfLikes / 100000;
 
-  var gamaScore = numberOfImpression / 1000000;
-  var xeetScore = numberOfLikes / 100000;
+  // Adjust scores based on extractorType and sniped flag
+  const multiplier = extractorType === "GOD" ? 10 : extractorType === "OG" ? (sniped ? 4 : 5) : 1;
 
-  if (extractorType === "GOD") {
-    xeetScore *= 10;
-    gamaScore *= 10;
-  } else if (extractorType === "OG") {
-  //sniped is a boolean to determin wether or not the url has been sniped by a sniper
-    if (!sniped) {
-      xeetScore *= 5;
-      gamaScore *= 5;
-    } else {
-      xeetScore *= 4;
-      gamaScore *= 4;
-    }
-  } else {
-    xeetScore;
-    gamaScore;
-  }
+  xeetScore *= multiplier;
+  gamaScore *= multiplier;
+
 
   return { xeetScore, gamaScore };
 };
