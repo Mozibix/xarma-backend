69995fac89af7c41977ef5cc128132df066cd8c2
Mozibix <opuzmoses@gmail.com>
2024-11-04 08:02:25 +0100

    chores: updated vaults

--- src/routes/vault.js
+++ src/routes/vault.js
@@ -4,11 +4,11 @@ import { hasAuth } from "../middlewares/hasAuth.js";
 
 const router = Router();
 
-router.post("/add-vault", VaultController.addVault);
-router.get("/:vaultId/get-vault", VaultController.getVaultById);
-router.get("/get-all-vaults", VaultController.getAllVaults);
-router.put("/:vaultId/update-vault", VaultController.updateVault);
-router.patch("/:vaultId/suspend-vault", VaultController.suspendVault);
-router.delete("/:vaultId/delete-vault", VaultController.deleteVault);
+router.post("/add-vault", hasAuth, VaultController.addVault);
+router.get("/:vaultId/get-vault", hasAuth, VaultController.getVaultById);
+router.get("/get-all-vaults", hasAuth, VaultController.getAllVaults);
+router.put("/:vaultId/update-vault", hasAuth, VaultController.updateVault);
+router.patch("/:vaultId/suspend-vault", hasAuth, VaultController.suspendVault);
+router.delete("/:vaultId/delete-vault", hasAuth, VaultController.deleteVault);
 
 export default router;
