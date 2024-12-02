ef1ad5b9b8da1cc324c112d300ec74ec8fd4f05d
Mozibix <opuzmoses@gmail.com>
2024-12-02 07:07:50 +0100

    chores: updated user ranks

--- src/enums/userRanks.js
+++ src/enums/userRanks.js
@@ -1,14 +1,15 @@
 const userRanks = {
-OBSERVER: 'observer',
-TRACKER: 'tracker',
-STRATEGIST: 'strategist',
-INFLUENCER: 'influencer',
-ARCHITECT: 'architect',
-INNOVATOR: 'innovator',
-VISIONARY: 'visionary',
-COMMANDER: 'commander',
-MASTERMIND: 'mastermind',
-CONQUEROR: 'conqueror',
-}
+  NORANK: "norank",
+  OBSERVER: "observer",
+  TRACKER: "tracker",
+  STRATEGIST: "strategist",
+  INFLUENCER: "influencer",
+  ARCHITECT: "architect",
+  INNOVATOR: "innovator",
+  VISIONARY: "visionary",
+  COMMANDER: "commander",
+  MASTERMIND: "mastermind",
+  CONQUEROR: "conqueror",
+};
 
 export default userRanks;
